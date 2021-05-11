98b1e51ea971ce8dc6a5e5cce8f08f89ddfcecc3
Your Name <kevin.kds80@gmail.com>
2021-05-11 19:47:44 -0300

    Removed integration with mini-kanban service from insertComment function

--- src/Services/InsertComment.hs
+++ src/Services/InsertComment.hs
@@ -4,17 +4,13 @@ module Services.InsertComment where
 
 import Web.Spock
 import Network.HTTP.Types
-import Network.HTTP.Simple
-import Network.HTTP.Conduit
 
 import BaseTypes.SpockApi ( Api, ApiAction )
 import qualified BaseTypes.PostRequest as PR
 import BaseTypes.Comment as CM
-import BaseTypes.TaskTypes as TT
 import Errors.ErrorMessages
 import Data.Maybe (isNothing)
 
-import qualified Data.Aeson as Aeson
 import Operations.Mongo.MongoDBOperations as MongoOperations
 import Control.Monad.Trans (liftIO)
 
@@ -23,6 +19,7 @@ import Data.UUID.V1 ( nextUUID )
 
 import LoadEnv
 import System.Environment (lookupEnv)
+import Services.Integrations.MiniKanban.GetTaskInfo
 
 insertComment :: Api
 insertComment = do
@@ -31,38 +28,29 @@ insertComment = do
         if isNothing body then
             setStatus status400 >> _PARSING_POST_BODY
         else do
-            liftIO loadEnv
-            maybeUrl <- liftIO (lookupEnv "CHECK_TASK_URL")
-            let (Just url) = maybeUrl
             let (Just sanitizeBody) = body
             let boardId = PR.boardId sanitizeBody
             let taskId = PR.taskId sanitizeBody
 
-            initReq <- liftIO $ parseRequest (url ++ "/" ++ boardId ++ "/" ++ taskId)
-            let req = initReq { method = "GET" }
-            response <- httpLBS req
-            let resCode = getResponseStatusCode response
+            taskIntegration <- liftIO $ getTaskInfo body
 
-            if resCode == 404 then do
-                setStatus status404 >> _BOARD_NOT_FOUND
-            else if resCode > 200 then do
+            if fst taskIntegration >= 500 then do
                 setStatus status500 >> _INTERNAL_SERVER_ERROR
+
+            else if snd taskIntegration == 404 then do
+                setStatus status404 >> _BOARD_NOT_FOUND
+
             else do 
-                let jsonBody = getResponseBody response
-                let decodedBody = Aeson.decode jsonBody :: Maybe TT.Response
-                case decodedBody of
-                    Nothing -> do setStatus status404 >> _BOARD_NOT_FOUND
-                    Just _ -> do
-                        uuid <- liftIO nextUUID
-                        let (Just sanitizedUUID) = uuid
-                        let strUUID = toString sanitizedUUID
+                uuid <- liftIO nextUUID
+                let (Just sanitizedUUID) = uuid
+                let strUUID = toString sanitizedUUID
 
-                        let comment = CM.Comment {
-                            CM.content   = PR.content sanitizeBody,
-                            CM.taskId    = taskId,
-                            CM.boardId   = boardId,
-                            CM.commentId = strUUID
-                        }
+                let comment = CM.Comment {
+                    CM.content   = PR.content sanitizeBody,
+                    CM.taskId    = taskId,
+                    CM.boardId   = boardId,
+                    CM.commentId = strUUID
+                }
 
-                        insertedComment <- liftIO $ MongoOperations.insertComment comment
-                        setStatus status201 >> json insertedComment
+                insertedComment <- liftIO $ MongoOperations.insertComment comment
+                setStatus status201 >> json insertedComment
