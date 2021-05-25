98da811e6d60f1ae884b51e89bc0c033e6c61a00
Your Name <kevin.kds80@gmail.com>
2021-05-25 19:59:50 -0300

    Add connection via parameters in service

--- src/Services/DeleteAllComments.hs
+++ src/Services/DeleteAllComments.hs
@@ -5,8 +5,9 @@ module Services.DeleteAllComments where
 import Operations.Mongo.MongoDBOperations as MongoOperations
 import Control.Monad.Trans (liftIO)
 import Operations.Mongo.ConnectionMongoDB
+import Database.MongoDB ( Pipe, Database )
 
-deleteAllComments field value = do
-    conn <- liftIO connection
-    liftIO $ MongoOperations.deleteComment conn field value
+deleteAllComments :: (Pipe, Database) -> String -> String -> IO ()
+deleteAllComments connection field value = do
+    liftIO $ MongoOperations.deleteComment connection field value
     putStrLn $ "Processed " ++ value
