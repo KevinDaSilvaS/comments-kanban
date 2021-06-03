9dd2dcaa1833286a35d7bc82f431562a3e6e1a84
Your Name <kevin.kds80@gmail.com>
2021-06-03 20:33:29 -0300

    Add update comment tests to spec

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,9 +3,11 @@ import Test.Hspec
 import Features.GetCommentSpec ( getCommentSpec )
 import Features.CreateCommentSpec ( createCommentSpec )
 import Features.GetAllCommentsSpec ( getAllCommentsSpec )
+import Features.UpdateCommentSpec ( updateCommentSpec )
 
 main :: IO ()
 main = do 
        createCommentSpec
        getAllCommentsSpec
        getCommentSpec
+       updateCommentSpec
