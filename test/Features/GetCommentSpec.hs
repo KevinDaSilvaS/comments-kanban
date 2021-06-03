4e0e7f708c817b9a3ce1117a4b00e044a29070f4
Your Name <kevin.kds80@gmail.com>
2021-06-03 19:37:09 -0300

    Add get one comment test spec

--- test/Features/GetCommentSpec.hs
+++ test/Features/GetCommentSpec.hs
@@ -3,18 +3,48 @@ import SpecHelper
 import Helpers.InsertComment
 import Control.Monad.Trans (liftIO)
 import Data.Maybe (isNothing)
-import BaseTypes.Comment ( Comment(taskId, boardId) )
+import BaseTypes.Comment 
+import Helpers.InsertComment ( insertComment )
+import qualified Helpers.ResponseTypes.SuccessSingleResponse as RR
+import qualified Helpers.ResponseTypes.ErrorResponse as RRE
+import Helpers.GetComment
 
-getCommentSpec = hspec $ do
+getCommentSuccessSpec :: IO ()
+getCommentSuccessSpec = hspec $ do
     describe "Success" $ do
-      it "Should get one comment" $ do
-        insertedComment <- (liftIO insertComment)
-        print insertedComment
-        (fst insertedComment) `shouldBe` (201 :: Int)
-        
-        isNothing (snd insertedComment) `shouldBe` (False :: Bool)
-        let (Just comment) = snd insertedComment
-        code comment `shouldBe` ("201" :: String)
-        taskId (details comment) `shouldBe` ("12s4f352j82sA8bB677" :: String)
-        boardId (details comment) `shouldBe` ("12s4f352j82sA8bB677" :: String)
+      it "Should get one comment successfully" $ do
+        let id = "12s4f352j82sA8bB677"
+        insertedComment <- liftIO (insertComment id)
 
+        fst insertedComment `shouldBe` (201 :: Int)
+        isNothing (snd insertedComment) `shouldBe` (False :: Bool)
+
+        let (Just comment) = snd insertedComment
+        let insertedCommentId = commentId (RR.details comment)
+
+        response <- liftIO (getComment id insertedCommentId)
+        fst response `shouldBe` (200 :: Int)
+  
+        isNothing (snd response) `shouldBe` (False :: Bool)
+        let (Just comment) = snd response
+        RR.code comment `shouldBe` ("200" :: String)
+        taskId (RR.details comment) `shouldBe` (id :: String)
+        boardId (RR.details comment) `shouldBe` (id :: String)
+        commentId (RR.details comment) `shouldBe` (insertedCommentId :: String)
+
+getCommentFailSpec :: IO ()
+getCommentFailSpec = hspec $ do
+  describe "Fail" $ do
+    it "Should get 404 comment not found" $ do
+        response <- liftIO (getCommentError "any_taskId" "any_commentId")
+        fst response `shouldBe` (404 :: Int)
+  
+        isNothing (snd response) `shouldBe` (False :: Bool)
+        let (Just errorMsg) = snd response
+        RRE.code errorMsg `shouldBe` ("404" :: String)
+        RRE.details errorMsg `shouldBe` ("Comment Not Found" :: String)
+
+getCommentSpec :: IO ()
+getCommentSpec = do
+  getCommentSuccessSpec
+  getCommentFailSpec
