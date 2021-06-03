fee4638a2dc3161a35f81d5d6045ddd5e5f01e8a
Your Name <kevin.kds80@gmail.com>
2021-06-03 19:41:17 -0300

    Add getCommentSpec to spec file

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,11 +1,11 @@
 import Test.Hspec
 
---import Features.GetCommentSpec (getCommentSpec)
-import Features.CreateCommentSpec
-import Features.GetAllCommentsSpec
+import Features.GetCommentSpec ( getCommentSpec )
+import Features.CreateCommentSpec ( createCommentSpec )
+import Features.GetAllCommentsSpec ( getAllCommentsSpec )
 
 main :: IO ()
 main = do 
        createCommentSpec
        getAllCommentsSpec
-       --getCommentSpec
+       getCommentSpec
