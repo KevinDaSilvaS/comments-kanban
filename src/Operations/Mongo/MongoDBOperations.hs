595e3ec2b1722829f9a3ed6b286b8d2ddf437d96
Your Name <kevin.kds80@gmail.com>
2021-10-30 14:48:12 -0300

    Add skip and limit to mongodb query

--- src/Operations/Mongo/MongoDBOperations.hs
+++ src/Operations/Mongo/MongoDBOperations.hs
@@ -11,58 +11,60 @@ import qualified BaseTypes.Comment as Com
 import LoadEnv
 import Data.Aeson (Object)
 import System.Environment (lookupEnv)
-import Control.Exception 
+import Control.Exception
+import Data.Word
 --https://github.com/mongodb-haskell/mongodb/blob/master/doc/Example.hs
 --https://subscription.packtpub.com/book/big_data_and_business_intelligence/9781783286331/1/ch01lvl1sec19/using-mongodb-queries-in-haskell
 --https://github.com/mongodb-haskell/mongodb/blob/master/doc/tutorial.md
 
 insertComment :: (Pipe, Database) -> Com.Comment -> IO Object
 insertComment (pipe, dbName) body = do
-    insertedCommentId <- access pipe master dbName 
+    insertedCommentId <- access pipe master dbName
         (insertCommentOperation body)
-    insertedComment <- access pipe master dbName 
-        (findCommentsOperation ["_id" =: insertedCommentId])
+    insertedComment <- access pipe master dbName
+        (findCommentsOperation ["_id" =: insertedCommentId] 0 1)
     return $ (aesonify . exclude ["_id"]) $ head insertedComment
 
-getAllComments :: (Pipe, Database) -> String -> IO [Object]
-getAllComments (pipe, dbName) taskId = do
-    comments <- access pipe master dbName (findCommentsOperation 
-        ["taskId" =: taskId])
+getAllComments :: (Pipe, Database) -> String -> Limit -> Limit-> IO [Object]
+getAllComments (pipe, dbName) taskId page limit = do
+    comments <- access pipe master dbName (findCommentsOperation
+        ["taskId" =: taskId] page limit)
     return $ map (aesonify . exclude ["_id"]) comments
 
 getComment :: (Pipe, Database) -> String -> String -> IO [Object]
 getComment (pipe, dbName) taskId commentId = do
-    comment <- access pipe master dbName 
+    comment <- access pipe master dbName
         (findCommentsOperation [
             "taskId" =: taskId, "commentId" =: commentId
-            ])
+            ] 0 1)
     return $ map (aesonify . exclude ["_id"]) comment
 
 updateComment :: (Pipe, Database) -> String -> String -> IO (Maybe SomeException)
 updateComment (pipe, dbName) commentId content = do
-    updatedComments <- try 
-        (access pipe master dbName 
-        (updateCommentsOperation commentId content)) 
+    updatedComments <- try
+        (access pipe master dbName
+        (updateCommentsOperation commentId content))
         :: IO (Either SomeException ())
-        
+
     case updatedComments of
         Left ex -> return $ Just ex
         Right _ -> return Nothing
 
 deleteComment :: (Pipe, Database) -> String -> String -> IO (Maybe SomeException)
 deleteComment (pipe, dbName) field value = do
-
-    deletedComments <- try $ 
-        access pipe master dbName 
-        (deleteCommentsOperation 
+    deletedComments <- try $
+        access pipe master dbName
+        (deleteCommentsOperation
         (T.pack field) value) :: IO (Either SomeException ())
     case deletedComments of
         Left ex -> return $ Just ex
         Right _ -> return Nothing
-    
-findCommentsOperation :: Selector -> Action IO [Document]
-findCommentsOperation query = do 
-    rest =<< find (select query "comments") {sort = []}
+
+findCommentsOperation :: Selector -> Limit -> Limit -> Action IO [Document]
+findCommentsOperation query page limit = do
+    rest =<< find
+        (select query "comments")
+        {sort = [], limit = limit, skip = page}
 
 insertCommentOperation :: Com.Comment -> Action IO Value
 insertCommentOperation Com.Comment {
