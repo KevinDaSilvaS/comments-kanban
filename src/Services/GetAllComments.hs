76fb4e4095369286278455f0b86d5364e0462aa6
Your Name <kevin.kds80@gmail.com>
2021-10-30 14:47:54 -0300

    Add pagination to getAllComments feature

--- src/Services/GetAllComments.hs
+++ src/Services/GetAllComments.hs
@@ -2,23 +2,40 @@
 
 module Services.GetAllComments where
 
-import Web.Spock
+import Web.Spock ( var, (<//>), get, param )
 
 import BaseTypes.Comment
 import BaseTypes.SpockApi ( Api, ApiAction )
 
-import Operations.Mongo.MongoDBOperations as MongoOperations
+import Operations.Mongo.MongoDBOperations as MongoOperations ( getAllComments )
 import Control.Monad.Trans (liftIO)
 
 import Response.Response as Res
 import Network.HTTP.Types
-import Database.MongoDB ( Pipe, Database )
-import Data.Text as T
-    
+import Database.MongoDB ( Pipe, Database, Limit )
+
 getAllComments :: (Pipe, Database) -> Api
 getAllComments connection = do
+    let defaultLimit = 50
+    let defaultPage  = 1
     get ("comments" <//> var) $ \taskId -> do
-        let pr = param ("comments", var) (T.pack "limit")
-        print pr
-        comments <- liftIO $ MongoOperations.getAllComments connection taskId
-        Res.response (statusCode status200) comments
+        maybePage  <- param "page" 
+        maybeLimit <- param "limit"
+        case maybePage of
+            Just page -> do
+                case maybeLimit of
+                    Just limit -> do 
+                        comments <- fetchResults 
+                            connection taskId ((page-1)*limit) limit
+                        Res.response (statusCode status200) comments
+                    _ -> do
+                        comments <- fetchResults
+                            connection taskId ((page-1)*defaultLimit) defaultLimit
+                        Res.response (statusCode status200) comments
+            _ -> do
+                comments <- 
+                    fetchResults connection taskId defaultPage defaultLimit
+                Res.response (statusCode status200) comments
+                
+fetchResults connection taskId page limit = 
+    liftIO $ MongoOperations.getAllComments connection taskId page limit
