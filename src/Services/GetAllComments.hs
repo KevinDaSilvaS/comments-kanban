b327fd6e04729da824c893c469ba8fad2c0fb495
Your Name <kevin.kds80@gmail.com>
2021-10-30 12:41:51 +0000

    Start testing params

--- src/Services/GetAllComments.hs
+++ src/Services/GetAllComments.hs
@@ -13,9 +13,12 @@ import Control.Monad.Trans (liftIO)
 import Response.Response as Res
 import Network.HTTP.Types
 import Database.MongoDB ( Pipe, Database )
+import Data.Text as T
     
 getAllComments :: (Pipe, Database) -> Api
 getAllComments connection = do
     get ("comments" <//> var) $ \taskId -> do
+        let pr = param ("comments", var) (T.pack "limit")
+        print pr
         comments <- liftIO $ MongoOperations.getAllComments connection taskId
         Res.response (statusCode status200) comments
