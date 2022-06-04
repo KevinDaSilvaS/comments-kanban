652ecd5a3d01ff73651984ae44b94cd7b9cafbb9
Your Name <kevin.kds80@gmail.com>
2022-06-04 15:33:26 -0300

    refactor service

--- src/Services/GetAllComments.hs
+++ src/Services/GetAllComments.hs
@@ -13,31 +13,14 @@ import Control.Monad.Trans (liftIO)
 import Response.Response as Res
 import Network.HTTP.Types
 import Database.MongoDB ( Pipe, Database, Limit )
+import Helpers.ValidatePagination
 
 getAllComments :: (Pipe, Database) -> Api
 getAllComments connection = do
-    let defaultLimit = 50
-    let defaultPage  = 1
     get ("comments" <//> var) $ \taskId -> do
         maybePage  <- param "page" 
         maybeLimit <- param "limit"
-        case maybePage of
-            Just page -> do
-                case maybeLimit of
-                    Just limit -> do 
-                        comments <- fetchResults connection taskId page limit
-                        Res.response (statusCode status200) comments
-                    _ -> do
-                        comments <- fetchResults 
-                            connection taskId page defaultLimit
-                        Res.response (statusCode status200) comments
-            _ -> do
-                comments <- 
-                    fetchResults connection taskId defaultPage defaultLimit
-                Res.response (statusCode status200) comments
-                
-fetchResults connection taskId page limit 
-    | page <= 0 = liftIO $ 
-        MongoOperations.getAllComments connection taskId 0 limit
-    | otherwise = liftIO $ 
-        MongoOperations.getAllComments connection taskId ((page-1)*limit) limit
+        let page  = obtainPage maybePage
+        let limit = obtainLimit maybeLimit
+        comments <- liftIO $ MongoOperations.getAllComments connection taskId (page*limit) limit
+        Res.response (statusCode status200) comments                
